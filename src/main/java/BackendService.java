1c8e7db1cec17b138a6fc156c2e890d110735ffd
MP351 <maxpayne351@gmail.com>
2018-10-23 10:15:45 +0300

    Update BackendService.java

--- src/main/java/BackendService.java
+++ src/main/java/BackendService.java
@@ -5,11 +5,5 @@ public class BackendService {
 
         Thread serverThread = new Thread(new SService());
         serverThread.start();
-        //System.out.println(manager.writeIntoIncome((long) 1515618545, 1000));
-        //System.out.println(manager.getSumForEnroll(new EnrollPeriod("2018-8")));
-        //manager.proceed(25000, System.currentTimeMillis());
-        //System.out.println(manager.getSumForEnroll(new EnrollPeriod("2018-2")));
-
-
     }
 }
